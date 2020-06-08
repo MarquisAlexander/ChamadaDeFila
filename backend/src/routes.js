ab38039c3507518d3c1df2b5a6725dea5c6bc7d3
MarquisAlexander <marquissantos123@gmail.com>
2020-06-07 21:07:36 -0300

    Alterações no frontend

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -2,10 +2,12 @@ const express = require('express');
 
 const CaixaController = require('./controllers/CaixaController');
 const PainelController = require('./controllers/PainelControler');
+const Painel01Controller = require('./controllers/Painel01Controller');
 
 const routes = express.Router('controletarefa.cb8wpsrs2beg.us-east-2.rds.amazonaws.com');
 
 routes.post('/caixa', CaixaController.create);
 routes.get('/painel', PainelController.index);
+routes.get('/painel1', Painel01Controller.index);
 
 module.exports = routes;
--- frontend/src/pages/Painel/index.js
+++ frontend/src/pages/Painel/index.js
@@ -1,10 +1,14 @@
 import React, { useState, useEffect } from 'react';
+import ReactPlayer from 'react-player';
 
 import api from '../../services/api'
+
+import './styles.css';
 
 export default function Caixa() {
 
     const [caixas, setCaixas] = useState([]);
+    const [caixas1, setCaixas1] = useState([]);
 
     useEffect(() => {
         api.get('painel', {
@@ -13,17 +17,55 @@ export default function Caixa() {
         })
     })
 
+    useEffect(() => {
+        api.get('painel1', {
+        }).then(response => {
+            setCaixas1(response.data)
+        })
+    })
+
     return (
         <>
-        <div>
-            <ul>
-                {caixas.map(caixa => (
-                    <li key={caixa.id}>
-                      <p>{caixa.nome}</p>  
-                    </li>   
-                ))}
-            </ul>
-            painel
+        <div className='painel'>
+            <div className='multi'>
+                <div className='logo'>
+                    <img src="https://www.lojaodascarnes.com.br/wp-content/uploads/2019/02/lojao-das-carnes-favicon.png" alt="logo"/>
+                </div>
+
+                <div className='caixa-player'>
+                    <ReactPlayer
+                    className='player-video'
+                    url='https://www.youtube.com/watch?v=zTitoHKsyJg'
+                    width= '100%'
+                    height= '100%' 
+                    />
+                </div>
+                <div className='horadata'>
+                    <p>18:30</p>
+                    <p>06/05/2020</p>
+                </div>
+            </div>
+
+
+
+            <div className='caixas'>
+                <strong>Caixa Livre</strong>
+                    <div className='caixas1'>
+                        {caixas1.map(caixa => (
+                            <li key={caixa.id}>
+                            <p>{caixa.n_caixa}</p>  
+                            </li>   
+                        ))}
+                    </div>
+                <div className='text'>Ultimas chamadas</div>
+                <ul>
+                    {caixas.map(caixa => (
+                        <li key={caixa.id}>
+                        <p>{caixa.n_caixa}</p>  
+                        </li>   
+                    ))}
+                </ul>
+            </div>
         </div>
         </>
     )
